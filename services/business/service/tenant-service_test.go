af02a17cc1ef0dfd119f92adf96b3f8ccd6e545c
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-09-24 06:01:06 +1200

    Unit test refactoring

--- services/business/service/tenant-service_test.go
+++ services/business/service/tenant-service_test.go
@@ -83,100 +83,99 @@ var _ = Describe("TenantTest Tests", func() {
 					Ω(ok).Should(BeTrue())
 					Ω(argumentErr.ArgumentName).Should(Equal("ctx"))
 				})
+			})
 
-				When("CreateTenant is called without request", func() {
-					It("should return ArgumentError", func() {
-						response, err := sut.CreateTenant(ctx, nil)
-						Ω(err).Should(HaveOccurred())
-						Ω(response).Should(BeNil())
+			When("CreateTenant is called without request", func() {
+				It("should return ArgumentError", func() {
+					response, err := sut.CreateTenant(ctx, nil)
+					Ω(err).Should(HaveOccurred())
+					Ω(response).Should(BeNil())
 
-						argumentErr, ok := err.(commonErrors.ArgumentError)
-						Ω(ok).Should(BeTrue())
-						Ω(argumentErr.ArgumentName).Should(Equal("request"))
-					})
+					argumentErr, ok := err.(commonErrors.ArgumentError)
+					Ω(ok).Should(BeTrue())
+					Ω(argumentErr.ArgumentName).Should(Equal("request"))
+				})
+			})
+
+			When("CreateTenant is called with invalid request", func() {
+				It("should return ArgumentError", func() {
+					invalidRequest := contract.CreateTenantRequest{
+						Tenant: models.Tenant{
+							Name: "",
+						}}
+
+					response, err := sut.CreateTenant(ctx, &invalidRequest)
+					Ω(err).Should(HaveOccurred())
+					Ω(response).Should(BeNil())
+
+					argumentErr, ok := err.(commonErrors.ArgumentError)
+					Ω(ok).Should(BeTrue())
+					Ω(argumentErr.ArgumentName).Should(Equal("request"))
+					Ω(argumentErr.ErrorMessage).Should(Equal(invalidRequest.Validate().Error()))
+				})
+			})
+
+			When("CreateTenant is called with correct input paramters", func() {
+				It("should call tenant respository CreateTenant method", func() {
+					mockTenantRepositoryService.
+						EXPECT().
+						CreateTenant(ctx, gomock.Any()).
+						Do(func(_ context.Context, mappedRequest *repositoryContract.CreateTenantRequest) {
+							Ω(mappedRequest.Tenant).Should(Equal(request.Tenant))
+						}).
+						Return(&repositoryContract.CreateTenantResponse{TenantID: cuid.New()}, nil)
+
+					_, err := sut.CreateTenant(ctx, &request)
+					Ω(err).Should(BeNil())
 				})
 
-				When("CreateTenant is called with invalid request", func() {
-					It("should return ArgumentError", func() {
-						invalidRequest := contract.CreateTenantRequest{
-							Tenant: models.Tenant{
-								Name: "",
-							}}
-
-						response, err := sut.CreateTenant(ctx, &invalidRequest)
-						Ω(err).Should(HaveOccurred())
-						Ω(response).Should(BeNil())
-
-						argumentErr, ok := err.(commonErrors.ArgumentError)
-						Ω(ok).Should(BeTrue())
-						Ω(argumentErr.ArgumentName).Should(Equal("request"))
-						Ω(argumentErr.ErrorMessage).Should(Equal(invalidRequest.Validate().Error()))
-					})
-				})
-
-				When("CreateTenant is called with correct input paramters", func() {
-					It("should call tenant respository CreateTenant method", func() {
+				When("And tenant repository CreateTenant return TenantAlreadyExistError", func() {
+					It("should return TenantAlreadyExistsError", func() {
 						mockTenantRepositoryService.
 							EXPECT().
-							CreateTenant(ctx, gomock.Any()).
-							Do(func(_ context.Context, mappedRequest *repositoryContract.CreateTenantRequest) {
-								Ω(mappedRequest.Tenant).Should(Equal(request.Tenant))
-							}).
-							Return(&repositoryContract.CreateTenantResponse{TenantID: cuid.New()}, nil)
+							CreateTenant(gomock.Any(), gomock.Any()).
+							Return(nil, repositoryContract.NewTenantAlreadyExistsError())
 
-						_, err := sut.CreateTenant(ctx, &request)
+						response, err := sut.CreateTenant(ctx, &request)
+						Ω(err).Should(HaveOccurred())
+						Ω(response).Should(BeNil())
+
+						_, ok := err.(contract.TenantAlreadyExistsError)
+						Ω(ok).Should(BeTrue())
+					})
+				})
+
+				When("And tenant repository CreateTenant return any error rather than TenantAlreadyExistsError", func() {
+					It("should return UnknownError", func() {
+						expectedError := errors.New(cuid.New())
+						mockTenantRepositoryService.
+							EXPECT().
+							CreateTenant(gomock.Any(), gomock.Any()).
+							Return(nil, expectedError)
+
+						response, err := sut.CreateTenant(ctx, &request)
+						Ω(err).Should(HaveOccurred())
+						Ω(response).Should(BeNil())
+
+						unknownErr, ok := err.(contract.UnknownError)
+						Ω(ok).Should(BeTrue())
+						Ω(unknownErr.ErrorMessage).Should(Equal(expectedError.Error()))
+					})
+				})
+
+				When("And tenant repository CreateTenant return no error", func() {
+					It("should return the new tenantID", func() {
+						tenantID := cuid.New()
+						mockTenantRepositoryService.
+							EXPECT().
+							CreateTenant(gomock.Any(), gomock.Any()).
+							Return(&repositoryContract.CreateTenantResponse{TenantID: tenantID}, nil)
+
+						response, err := sut.CreateTenant(ctx, &request)
 						Ω(err).Should(BeNil())
+						Ω(response).ShouldNot(BeNil())
+						Ω(response.TenantID).Should(Equal(tenantID))
 					})
-
-					When("And tenant repository CreateTenant return TenantAlreadyExistError", func() {
-						It("should return TenantAlreadyExistsError", func() {
-							mockTenantRepositoryService.
-								EXPECT().
-								CreateTenant(gomock.Any(), gomock.Any()).
-								Return(nil, repositoryContract.NewTenantAlreadyExistsError())
-
-							response, err := sut.CreateTenant(ctx, &request)
-							Ω(err).Should(HaveOccurred())
-							Ω(response).Should(BeNil())
-
-							_, ok := err.(contract.TenantAlreadyExistsError)
-							Ω(ok).Should(BeTrue())
-						})
-					})
-
-					When("And tenant repository CreateTenant return any error rather than TenantAlreadyExistsError", func() {
-						It("should return UnknownError", func() {
-							expectedError := errors.New(cuid.New())
-							mockTenantRepositoryService.
-								EXPECT().
-								CreateTenant(gomock.Any(), gomock.Any()).
-								Return(nil, expectedError)
-
-							response, err := sut.CreateTenant(ctx, &request)
-							Ω(err).Should(HaveOccurred())
-							Ω(response).Should(BeNil())
-
-							unknownErr, ok := err.(contract.UnknownError)
-							Ω(ok).Should(BeTrue())
-							Ω(unknownErr.ErrorMessage).Should(Equal(expectedError.Error()))
-						})
-					})
-
-					When("And tenant repository CreateTenant return no error", func() {
-						It("should return the new tenantID", func() {
-							tenantID := cuid.New()
-							mockTenantRepositoryService.
-								EXPECT().
-								CreateTenant(gomock.Any(), gomock.Any()).
-								Return(&repositoryContract.CreateTenantResponse{TenantID: tenantID}, nil)
-
-							response, err := sut.CreateTenant(ctx, &request)
-							Ω(err).Should(BeNil())
-							Ω(response).ShouldNot(BeNil())
-							Ω(response.TenantID).Should(Equal(tenantID))
-						})
-					})
-
 				})
 			})
 		})
