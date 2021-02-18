70ce97cd7db2b735a0cf87e3bea649141779428b
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2021-02-18 17:22:57 +1300

    Upgraded to golang 1.16 (#127)

--- pkg/util/start.go
+++ pkg/util/start.go
@@ -12,7 +12,7 @@ import (
 	"github.com/decentralized-cloud/project/services/repository/mongodb"
 	"github.com/decentralized-cloud/project/services/transport/grpc"
 	"github.com/decentralized-cloud/project/services/transport/https"
-	"github.com/micro-business/gokit-core/middleware"
+	"github.com/micro-business/go-core/gokit/middleware"
 	"go.uber.org/zap"
 )
 
